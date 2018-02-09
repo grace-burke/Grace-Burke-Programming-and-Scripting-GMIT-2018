050201b14d28b76c61b96dcc881aca1a4de072cb
grace-burke <35901108+grace-burke@users.noreply.github.com>
2018-02-09 00:08:48 +0000

    Create collatzexercise.py

---
+++ collatzexercise.py
@@ -0,0 +1,12 @@
+# Grace Burke 08/02/2018
+# https://en.wikipedia.org/wiki/Collatz_conjecture
+
+x = int(input("Please enter an integer: "))
+
+while x > 1:
+    if x % 2 == 0:
+        x = x/2
+    else:
+        x = 3*x + 1
+    print (int(x))
+    
