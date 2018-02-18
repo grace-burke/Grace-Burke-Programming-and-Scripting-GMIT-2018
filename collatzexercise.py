3041c668a8ee4d974acfd7d88a9917b3b8916a11
grace-burke <35901108+grace-burke@users.noreply.github.com>
2018-02-18 21:04:06 +0000

    Update collatzexercise.py

--- collatzexercise.py
+++ collatzexercise.py
@@ -5,8 +5,8 @@ x = int(input("Please enter an integer: "))
 
 while x > 1:
     if x % 2 == 0:
-        x = x/2
+        x = x//2
     else:
         x = 3*x + 1
-    print (int(x))
+    print (x)
     
