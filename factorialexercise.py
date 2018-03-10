c438b9c77ff06bdba56f6b59ed09b1bce6efa541
Grace Burke <g00364750@gmit.it>
2018-03-10 15:28:11 +0000

    Renamed function

--- factorialexercise.py
+++ factorialexercise.py
@@ -4,12 +4,12 @@
 # Write a Python script containing a function called factorial() that takes a single input/argument which is a positive integer and returns its factorial.
 # You should, in your script, test the function by calling it with the values 5, 7, and 10.
 
-def factorialfunction(x):
+def factorial(x):
     result = 1
     for i in range(1, (x+1)):
         result = result * i
     return result
 
-print ("The factorial of 5 is", factorialfunction(5))
-print ("The factorial of 7 is", factorialfunction(7))
-print ("The factorial of 10 is", factorialfunction(10))
+print ("The factorial of 5 is", factorial(5))
+print ("The factorial of 7 is", factorial(7))
+print ("The factorial of 10 is", factorial(10))
